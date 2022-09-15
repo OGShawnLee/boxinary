6497cb62e7e71d723fea30033930c3f6528b054e
Shawn Lee <changoloco.evg@gmail.com>
2022-09-15 17:26:22 -0500

    feat(ui): improve submit button design

--- src/routes/auth/sign-up/+page.svelte
+++ src/routes/auth/sign-up/+page.svelte
@@ -11,7 +11,7 @@
 <h1 class="sr-only">Sign Up</h1>
 
 <main>
-	<form method="post">
+	<form class="grid gap-9" method="post">
 		<div class="grid gap-6">
 			<InputGroup id="name" charLimit={50} error={form?.name} />
 			<InputGroup id="display-name" label="Display Name" charLimit={16} error={form?.displayName} />
@@ -22,6 +22,11 @@
 				<input type="password" {...input} use:bind />
 			</InputGroup>
 		</div>
-		<button type="submit"> Submit </button>
+		<button
+			class="min-h-10 px-6 | grid place-content-center | border-2 border-aqua-50 rounded-xl font-bold text-white"
+			type="submit"
+		>
+			Sign Up
+		</button>
 	</form>
 </main>
