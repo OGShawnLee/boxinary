4a953fb16243379e1d95a2a28a01b080b4d4c707
Shawn Lee <changoloco.evg@gmail.com>
2022-09-15 18:33:22 -0500

    refactor(ts): move global types to global.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,21 +7,3 @@ declare namespace App {
 	// interface PageError {}
 	// interface Platform {}
 }
-
-interface JWTPayloadState {
-	id: number;
-	email: string;
-	name: string;
-}
-
-type Optional<T> = { [V in keyof T]?: T[V] };
-
-type ValidationUnit<T = string> =
-	| T
-	| Optional<Record<"duplicate" | "incorrect" | "invalid" | "missing" | "not-found", boolean>>;
-
-type ValidationForm<T extends string> =
-	| {
-			[K in T]: ValidationUnit;
-	  }
-	| null;
---
+++ src/global.d.ts
@@ -0,0 +1,17 @@
+interface JWTPayloadState {
+	id: number;
+	email: string;
+	name: string;
+}
+
+type Optional<T> = { [V in keyof T]?: T[V] };
+
+type ValidationUnit<T = string> =
+	| T
+	| Optional<Record<"duplicate" | "incorrect" | "invalid" | "missing" | "not-found", boolean>>;
+
+type ValidationForm<T extends string> =
+	| {
+			[K in T]: ValidationUnit;
+	  }
+	| null;
