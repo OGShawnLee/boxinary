b8180a380a1247b8b148d683a128f70b54936bed
Shawn Lee <changoloco.evg@gmail.com>
2022-09-15 18:23:30 -0500

    feat(route): redirect from landing page to /home if user is logged in

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,15 @@
+import type { ServerLoad } from "@sveltejs/kit";
+import { AUTH_COOKIE } from "$env/static/private";
+import { useAwait } from "$lib/hooks";
+import { redirect } from "@sveltejs/kit";
+import { deleteAuthCookie } from "@root/server/utils";
+import { isLoggedIn } from "@root/server/validation";
+
+export const load: ServerLoad = async ({ cookies }) => {
+	const [isSignedIn, err] = await useAwait(() => isLoggedIn(cookies.get(AUTH_COOKIE)));
+	if (isSignedIn) throw redirect(303, "/home");
+	if (err) {
+		deleteAuthCookie(cookies);
+		throw redirect(303, "/auth/sign-in");
+	}
+};
--- src/server/validation/index.ts
+++ src/server/validation/index.ts
@@ -1,3 +1,6 @@
+import { ACCESS_TOKEN } from "$env/static/private";
+import { verify } from "jsonwebtoken";
+import { isEmpty } from "malachite-ui/predicate";
 import { isInterface, isNumber, isString } from "malachite-ui/predicate";
 import { compare } from "bcrypt";
 import { useAwait } from "$lib/hooks";
@@ -7,6 +10,11 @@ export function isIncorrectPassword(data: string | Buffer, password: string) {
 		const isCorrectPassword = await compare(data, password);
 		return !isCorrectPassword;
 	});
+}
+export async function isLoggedIn(authStateCookie: string | void) {
+	if (authStateCookie === undefined || isEmpty(authStateCookie)) return false;
+	const authState = verify(authStateCookie, ACCESS_TOKEN);
+	return isJWTPayloadState(authState);
 }
 
 export function isJWTPayloadState(val: unknown): val is JWTPayloadState {
