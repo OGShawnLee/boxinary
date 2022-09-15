9be950df9a6f1a5ad9a05a6d529d15b428829325
Shawn Lee <changoloco.evg@gmail.com>
2022-09-14 23:24:22 -0500

    feat(route): add primitive sign-up page

---
+++ src/routes/auth/sign-up/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { InputGroup } from "$lib/components";
+
+	export let form: ValidationForm<"name" | "displayName" | "email" | "password">;
+</script>
+
+<svelte:head>
+	<title>Boxinary / Sign Up</title>
+</svelte:head>
+
+<h1 class="sr-only">Sign Up</h1>
+
+<main>
+	<form method="post">
+		<div class="grid gap-6">
+			<InputGroup id="name" charLimit={50} error={form?.name} />
+			<InputGroup id="display-name" label="Display Name" charLimit={16} error={form?.displayName} />
+			<InputGroup id="email" charLimit={254} error={form?.email} let:input>
+				<input type="email" {...input} />
+			</InputGroup>
+			<InputGroup id="password" charLimit={60} error={form?.password} let:input>
+				<input type="password" {...input} />
+			</InputGroup>
+		</div>
+		<button type="submit"> Submit </button>
+	</form>
+</main>
