38afd88e940767ca9d9a0ddcd19e18635feb9957
Shawn Lee <changoloco.evg@gmail.com>
2022-09-16 10:39:59 -0500

    refactor: move layout navbar to its own component

---
+++ src/lib/layout/NavBar.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import type { ClientUser } from "@root/app";
+	import type { Nullable } from "malachite-ui/types";
+	import { ListItemLink } from "$lib/components";
+
+	export let currentUser: Nullable<ClientUser> = undefined;
+</script>
+
+<header
+	class="fixed top-0 inset-x-0 h-24 | flex items-center | bg-raisin-10/85 backdrop-filter backdrop-blur-sm | lg:static"
+>
+	<div class="w-full max-w-6xl mx-auto px-4 | flex items-center justify-between | lg:px-0">
+		<div class="flex items-center gap-24">
+			<a href="/">
+				<span class="font-bold text-4xl text-white"> Boxinary </span>
+			</a>
+			<nav class="hidden | lg:block">
+				<ul class="flex items-center gap-12 | font-medium">
+					<ListItemLink class="hover:text-white" href="/home" text="Home" />
+					<ListItemLink class="hover:text-white" href="/documentation" text="Documentation" />
+					<ListItemLink class="hover:text-white" href="/community" text="Community" />
+					<ListItemLink class="hover:text-white" href="/dictionary" text="Dictionary" />
+				</ul>
+			</nav>
+		</div>
+		<div class="hidden | lg:(flex gap-3)">
+			{#if currentUser}
+				<div class="flex items-center gap-6">
+					<div class="grid">
+						<span class="text-sm text-white font-bold"> {currentUser.name} </span>
+						<span class="text-xs font-medium"> @{currentUser.displayName} </span>
+					</div>
+					<form method="post" action="/auth/sign-out">
+						<button
+							class="min-h-10 px-6 | border-2 border-raisin-20 rounded-xl text-rich-90 font-medium"
+						>
+							Sign Out
+						</button>
+					</form>
+				</div>
+			{:else}
+				<a
+					class="min-h-10 px-6 | grid place-content-center | border-2 border-aqua-50 rounded-xl font-bold text-white"
+					href="/auth/sign-up"
+				>
+					Sign Up
+				</a>
+				<a
+					class="min-h-10 px-6 | grid place-content-center | border-2 border-raisin-20 rounded-xl text-rich-90 font-medium"
+					href="/auth/sign-in"
+				>
+					Sign In
+				</a>
+			{/if}
+		</div>
+	</div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import "virtual:windi.css";
 	import type { LayoutData } from "./$types";
-	import { ListItemLink } from "$lib/components";
+	import { NavBar } from "$lib/layout";
 
 	export let data: LayoutData;
 </script>
@@ -10,59 +10,7 @@
 	<title>Boxinary</title>
 </svelte:head>
 
-<header
-	class="fixed top-0 inset-x-0 h-24 | flex items-center | bg-raisin-10/85 backdrop-filter backdrop-blur-sm | lg:static"
->
-	<div class="w-full max-w-6xl mx-auto px-4 | flex items-center justify-between | lg:px-0">
-		<div class="flex items-center gap-24">
-			<a href="/">
-				<span class="font-bold text-4xl text-white"> Boxinary </span>
-			</a>
-			<nav class="hidden | lg:block">
-				<ul class="flex items-center gap-12 | font-medium">
-					<ListItemLink class="hover:text-white" href="/home" text="Home" />
-					<ListItemLink class="hover:text-white" href="/documentation" text="Documentation" />
-					<ListItemLink class="hover:text-white" href="/community" text="Community" />
-					<ListItemLink class="hover:text-white" href="/dictionary" text="Dictionary" />
-				</ul>
-			</nav>
-		</div>
-		<div class="hidden | lg:(flex gap-3)">
-			{#if data.user}
-				<div class="flex items-center gap-6">
-					<div class="grid">
-						<span class="text-sm text-white font-bold">
-							{data.user.name}
-						</span>
-						<span class="text-xs font-medium">
-							@{data.user.displayName}
-						</span>
-					</div>
-					<form method="post" action="/auth/sign-out">
-						<button
-							class="min-h-10 px-6 | border-2 border-raisin-20 rounded-xl text-rich-90 font-medium"
-						>
-							Sign Out
-						</button>
-					</form>
-				</div>
-			{:else}
-				<a
-					class="min-h-10 px-6 | grid place-content-center | border-2 border-aqua-50 rounded-xl font-bold text-white"
-					href="/auth/sign-up"
-				>
-					Sign Up
-				</a>
-				<a
-					class="min-h-10 px-6 | grid place-content-center | border-2 border-raisin-20 rounded-xl text-rich-90 font-medium"
-					href="/auth/sign-in"
-				>
-					Sign In
-				</a>
-			{/if}
-		</div>
-	</div>
-</header>
+<NavBar currentUser={data.user} />
 
 <div class="w-full max-w-6xl mx-auto my-12 | <lg:(px-4 mt-32)">
 	<slot />
