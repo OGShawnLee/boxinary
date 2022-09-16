dba77aebec278d3e2fe41db86ca27b44df628ce7
Shawn Lee <changoloco.evg@gmail.com>
2022-09-16 10:07:51 -0500

    fix(route): add /[user] page title

--- src/routes/[displayName]/+page.svelte
+++ src/routes/[displayName]/+page.svelte
@@ -4,6 +4,10 @@
 	export let data: PageData;
 </script>
 
+<svelte:head>
+	<title>{data.foundUser.name} (@{data.foundUser.displayName}) / Boxinary</title>
+</svelte:head>
+
 <header class="grid">
 	<h1 class="text-lg text-white font-bold">{data.foundUser.name}</h1>
 	<span class="font-medium"> @{data.foundUser.displayName} </span>
