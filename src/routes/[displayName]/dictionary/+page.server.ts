f8151c486eecd62fb3f1c3148ba0d421cc754fd2
Shawn Lee <changoloco.evg@gmail.com>
2022-09-21 22:09:33 -0500

    fix(route): send 404 if user does not exit
    bug: loading /[user]/dictionary with a non-existent user displayName
    would load a normal page instead of sending a 404 error page

--- src/routes/[displayName]/dictionary/+page.server.ts
+++ src/routes/[displayName]/dictionary/+page.server.ts
@@ -1,8 +1,12 @@
 import type { PageServerLoad } from "./$types";
 import { error } from "@sveltejs/kit";
-import { getUserDefinitions } from "@root/server/services";
+import { getUserByDisplayName, getUserDefinitions } from "@root/server/services";
 
 export const load: PageServerLoad = async ({ params: { displayName } }) => {
+	const [foundUser, err] = await getUserByDisplayName(displayName);
+	if (err) throw error(500, { message: "Unable to Get User Information" });
+	if (!foundUser) throw error(404, { message: "User not Found" });
+
 	const [definitions] = await getUserDefinitions(displayName);
 	if (definitions) return { definitions };
 	throw error(500, { message: "Unable to Get User Definitions" });
