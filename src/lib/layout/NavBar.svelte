e468f4b62ae41b5006e1b5ffa932a548246f4fc6
Shawn Lee <changoloco.evg@gmail.com>
2022-10-09 09:25:59 -0500

    fix(ui): reduce NavBar logo and navigation links gap

--- src/lib/layout/NavBar.svelte
+++ src/lib/layout/NavBar.svelte
@@ -11,7 +11,7 @@
 	class="fixed top-0 inset-x-0 z-90 h-16 | flex items-center | bg-raisin-10/85 backdrop-filter backdrop-blur-sm | md:h-24 lg:static"
 >
 	<div class="w-full max-w-6xl mx-auto px-4 | flex items-center justify-between | lg:px-0">
-		<div class="flex items-center gap-24">
+		<div class="flex items-center gap-12">
 			<a class="text-2xl text-white font-medium font-poppins md:text-4xl" href="/"> Boxinary </a>
 			<nav class="hidden | lg:block">
 				<ul class="flex items-center gap-12 | font-medium">
