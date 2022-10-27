d224ccfe68ad5fef56fcc4247c695dde2acde583
Shawn Lee <changoloco.evg@gmail.com>
2022-10-27 13:26:05 -0500

    feat(ui): improve CardUser design
    - improve mobile view
    - move link to displayName

--- src/lib/components/CardUser.svelte
+++ src/lib/components/CardUser.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { formatUserJoinDate, getProfilePath } from "$lib/utils";
+	import { getFormatedDate, getProfilePath } from "$lib/utils";
 
 	export let name: string;
 	export let displayName: string;
@@ -8,14 +8,18 @@
 
 <article class="px-6 py-3 | bg-raisin-12 rounded-md">
 	<header>
-		<h2 class="text-lg text-white font-medium">
-			<a class="hover:text-aqua-50" href={getProfilePath(displayName)} data-sveltekit-prefetch>
-				{name}
+		<h2 class="text-lg text-rich-90 font-medium">{name}</h2>
+		<div class="flex items-end justify-between">
+			<a
+				class="max-w-fit | text-sm hover:(text-aqua-50 underline)"
+				href={getProfilePath(displayName)}
+				data-sveltekit-prerender
+			>
+				@{displayName}
 			</a>
-		</h2>
-		<div class="flex items-end justify-between gap-6">
-			<span class="text-sm">@{displayName}</span>
-			<span class="text-xs text-rich-50">{formatUserJoinDate(createdAt)}</span>
+			<span class="text-xs text-rich-50">
+				Joined {getFormatedDate(createdAt)}
+			</span>
 		</div>
 	</header>
 </article>
