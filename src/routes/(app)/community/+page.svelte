39a47f0de7c137d552e6ce77dac4ff8aa8e9f4cd
Shawn Lee <changoloco.evg@gmail.com>
2022-10-27 13:25:23 -0500

    fix(ui): improve /community responsive design

--- src/routes/(app)/community/+page.svelte
+++ src/routes/(app)/community/+page.svelte
@@ -16,7 +16,7 @@
 <main>
 	<section class="grid gap-9">
 		<h2 class="text-2xl text-white font-bold">Latest Users</h2>
-		<div class="grid grid-cols-3 gap-6">
+		<div class="grid gap-6 | md:grid-cols-2 xl:grid-cols-3">
 			{#each foundUsers as user (user.displayName)}
 				<CardUser {...user} />
 			{/each}
