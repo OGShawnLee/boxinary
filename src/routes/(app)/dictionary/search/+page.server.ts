6a53aa8482e22e385c32cc0a948a415880cf0ad5
Shawn Lee <changoloco.evg@gmail.com>
2022-11-05 14:43:57 -0600

    refactor: simplify code

--- src/routes/(app)/dictionary/search/+page.server.ts
+++ src/routes/(app)/dictionary/search/+page.server.ts
@@ -1,7 +1,6 @@
 import type { PageServerLoad } from "./$types";
-import db from "$lib/db";
 import { error } from "@sveltejs/kit";
-import { useAwait } from "$lib/hooks";
+import { findDefinitionsByName } from "@server/services";
 import { isEmpty, isNullish } from "malachite-ui/predicate";
 
 export const load: PageServerLoad = async ({ url }) => {
@@ -11,13 +10,3 @@ export const load: PageServerLoad = async ({ url }) => {
 	if (results) return { name: name.toLowerCase(), results };
 	throw error(500, { message: "Unable to Find Definitions" });
 };
-
-function findDefinitionsByName(name: string) {
-	return useAwait(() =>
-		db.definition.findMany({
-			where: { name: { startsWith: name } },
-			include: { user: { select: { displayName: true } } },
-			orderBy: { name: "asc" }
-		})
-	);
-}
