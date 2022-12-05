82e9af535b13a21ec44e254a47454cc2a8a07fe0
Shawn Lee <changoloco.evg@gmail.com>
2022-12-04 21:37:50 -0600

    fix(ui): reduce body top-margin for mobile view

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -5,6 +5,6 @@
 
 <NavBar currentUser={$currentUser} />
 
-<div class="w-full mx-auto mt-24 mb-12 px-4 | layout-length | lg:(mt-6 px-0)">
+<div class="w-full mx-auto mt-16 mb-12 px-4 | layout-length | md:(mt-24) lg:(mt-6 px-0)">
 	<slot />
 </div>
