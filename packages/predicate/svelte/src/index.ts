de886f747a9431f5e32ce9eac071281782120732
Shawn Lee <changoloco.evg@gmail.com>
2022-12-11 15:32:11 -0600

    predicate(svelte): add isWritable fn

--- packages/predicate/svelte/src/index.ts
+++ packages/predicate/svelte/src/index.ts
@@ -1,8 +1,16 @@
-import type { Readable } from "svelte/store";
+import type { Readable, Writable } from "svelte/store";
 import { isFunction, isInterface } from "@boxinary/predicate-core";
 
 export function isStore(value: unknown): value is Readable<any> {
   return isInterface<Readable<any>>(value, {
     subscribe: isFunction,
+  });
+}
+
+export function isWritable(value: unknown): value is Writable<any> {
+  return isInterface<Writable<any>>(value, {
+    subscribe: isFunction,
+    set: isFunction,
+    update: isFunction,
   });
 }
--- packages/predicate/svelte/test/index.test.ts
+++ packages/predicate/svelte/test/index.test.ts
@@ -1,5 +1,5 @@
 import { derived, readable, writable } from "svelte/store";
-import { isStore } from "$lib";
+import { isStore, isWritable } from "$lib";
 
 describe("isStore", () => {
   it("Should return true with a valid store (readable, derived and writable)", () => {
@@ -27,13 +27,43 @@ describe("isStore", () => {
   });
 });
 
+describe("isWritable", () => {
+  it("Should return true with writable stores", () => {
+    const count = writable(10);
+    expect(isWritable(count)).toBe(true);
+  });
+
+  it("Should work with custom stores", () => {
+    const counter = createCounter(0);
+    expect(isWritable(counter)).toBe(true);
+  });
+
+  it("Should return false with readable stores (derived or readable)", () => {
+    const count = writable(10);
+    const double = derived(count, (count) => count * 2);
+    const read = readable(0, (set) => double.subscribe(set));
+    expect(isWritable(count)).toBe(true);
+    expect(isWritable(double)).toBe(false);
+    expect(isWritable(read)).toBe(false);
+  });
+
+  it("Should return false if any store method is not a function", () => {
+    const nonStore = { subscribe: () => {}, set: 10, update: "10" };
+    expect(isWritable(nonStore)).toBe(false);
+  });
+
+  it("Shoul return false with anything else", () => {
+    const values = [0, "0", {}, [], true, false, undefined, null];
+    for (const value of values) expect(isWritable(value)).toBe(false);
+  });
+});
+
 function createCounter(initialValue: number) {
-  const { subscribe, set, update } = writable(initialValue);
+  const count = writable(initialValue);
   return {
-    subscribe,
-    set,
+    ...count,
     increment() {
-      update((count) => count + 1);
+      count.update((count) => count + 1);
     },
   };
 }
