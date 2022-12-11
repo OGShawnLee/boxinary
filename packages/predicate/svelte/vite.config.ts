1abb86bff1a18ed239e5820eb55e76e4472b6cd3
Shawn Lee <changoloco.evg@gmail.com>
2022-12-11 15:20:44 -0600

    predicate(svelte): configure package

--- packages/predicate/svelte/vite.config.ts
+++ packages/predicate/svelte/vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from "vitest/config"
-import { resolve } from 'path'
+import dts from "vite-plugin-dts";
+import { defineConfig } from "vitest/config";
+import { resolve } from "path";
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "predicate",
+      fileName: "index",
+      formats: ["cjs", "es", "iife", "umd"],
+    },
+  },
+  plugins: [dts({ insertTypesEntry: true })],
   resolve: {
     alias: {
-      "$lib": resolve("src")
-    }
+      $lib: resolve("src"),
+    },
   },
   test: {
-    globals: true
-  }
-})
+    globals: true,
+  },
+});
