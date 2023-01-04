f8605ae3ca6bd9762d75c3db0bade6de18df807c
Shawn Lee <changoloco.evg@gmail.com>
2023-01-04 12:39:13 -0600

    ui(route): update navigable page

--- packages/ui/src/routes/navigable/+page.svelte
+++ packages/ui/src/routes/navigable/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { Page } from "@app/components";
 	import { Navigable, NavigableItem } from "$lib";
 
 	let disabled = false;
@@ -7,41 +8,37 @@
 	let finite = false;
 </script>
 
-<main class="grid gap-16">
-	<section class="grid gap-6">
-		<h2 class="text-2xl text-white font-semibold">Options</h2>
-		<div class="flex justify-between gap-3">
-			<button
-				class="button focus:border-slate-400"
-				class:border-purple-500={finite}
-				on:click={() => (finite = !finite)}
-			>
-				Toggle Finite
-			</button>
-			<button
-				class="button focus:border-slate-400"
-				class:border-purple-500={global}
-				on:click={() => (global = !global)}
-			>
-				Toggle Global
-			</button>
-			<button
-				class="button focus:border-slate-400"
-				class:border-purple-500={vertical}
-				on:click={() => (vertical = !vertical)}
-			>
-				Toggle Vertical
-			</button>
-			<button
-				class="button focus:border-slate-400"
-				class:border-purple-500={disabled}
-				on:click={() => (disabled = !disabled)}
-			>
-				Press D to Toggle Disabled Item
-			</button>
-		</div>
-	</section>
-
+<Page title="Navigable">
+	<svelte:fragment slot="options">
+		<button
+			class="button focus:border-slate-400"
+			class:border-purple-500={finite}
+			on:click={() => (finite = !finite)}
+		>
+			Toggle Finite
+		</button>
+		<button
+			class="button focus:border-slate-400"
+			class:border-purple-500={global}
+			on:click={() => (global = !global)}
+		>
+			Toggle Global
+		</button>
+		<button
+			class="button focus:border-slate-400"
+			class:border-purple-500={vertical}
+			on:click={() => (vertical = !vertical)}
+		>
+			Toggle Vertical
+		</button>
+		<button
+			class="button focus:border-slate-400"
+			class:border-purple-500={disabled}
+			on:click={() => (disabled = !disabled)}
+		>
+			Press D to Toggle Disabled Item
+		</button>
+	</svelte:fragment>
 	<div class="grid gap-12">
 		<Navigable class="flex justify-between" {finite} {vertical}>
 			<NavigableItem class="button button--focus">First Item</NavigableItem>
@@ -60,4 +57,4 @@
 			<NavigableItem class="button button--disabled" disabled>Fifth Item</NavigableItem>
 		</Navigable>
 	</div>
-</main>
+</Page>
