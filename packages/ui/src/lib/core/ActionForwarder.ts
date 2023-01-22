181dc7fface373799007e495429c57e898d06880
Shawn Lee <changoloco.evg@gmail.com>
2023-01-22 12:05:11 -0600

    ui: update ActionForwarder

--- packages/ui/src/lib/core/ActionForwarder.ts
+++ packages/ui/src/lib/core/ActionForwarder.ts
@@ -4,68 +4,54 @@ interface Action {
 	(element: HTMLElement): void | { destroy?: Unsubscriber };
 }
 
-namespace Forwarder {
-	export interface CachedAction {
-		destroy?: Unsubscriber;
-	}
+interface CachedAction {
+	destroy?: Unsubscriber;
 }
 
 class ActionForwarder {
 	protected readonly element: HTMLElement;
-	protected readonly cache: Map<Action, Forwarder.CachedAction>;
+	protected readonly cache = new Map<Action, CachedAction>();
 
-	constructor(element: HTMLElement, actions: Array<Action>) {
+	constructor(element: HTMLElement, actions: Action[]) {
 		this.element = element;
-		this.cache = new Map<Action, Forwarder.CachedAction>();
-		this.initialiseActions(actions);
+		for (const action of actions) this.add(action);
 	}
 
-	protected add(this: ActionForwarder, action: Action) {
+	protected add(action: Action) {
 		const result = action(this.element);
-		this.cache.set(action, {
-			destroy: result ? result.destroy : undefined
-		});
+		this.cache.set(action, { destroy: result?.destroy });
 	}
 
-	protected delete(this: ActionForwarder, action: Action) {
+	protected delete(action: Action) {
 		const cached = this.cache.get(action);
 		cached?.destroy?.();
 	}
 
 	onDestroy(this: ActionForwarder) {
-		for (const action of this.cache.values()) {
-			action.destroy?.();
-		}
+		for (const action of this.cache.values()) action.destroy?.();
 	}
 
-	onUpdate(this: ActionForwarder, actions: Array<Action>) {
-		const preserved = new Set<Action>();
-
+	onUpdate(this: ActionForwarder, actions: Action[]) {
+		const preservedActions = new Set<Action>();
 		for (const action of actions) {
-			if (this.isCachedAction(action)) continue;
-			this.add(action);
-			preserved.add(action);
+			if (this.isNewAction(action)) this.add(action);
+			preservedActions.add(action);
 		}
-
-		for (const cached of this.cache.keys()) {
-			if (preserved.has(cached)) continue;
-			this.delete(cached);
+		for (const cachedAction of this.cache.keys()) {
+			if (preservedActions.has(cachedAction)) continue;
+			this.delete(cachedAction);
 		}
 	}
 
-	protected isCachedAction(this: ActionForwarder, action: Action) {
+	protected isNewAction(action: Action) {
 		return this.cache.has(action);
-	}
-
-	protected initialiseActions(this: ActionForwarder, actions: Array<Action>) {
-		for (const action of actions) this.add(action);
 	}
 }
 
-export function forward(element: HTMLElement, actions: Array<Action>) {
+export function forward(element: HTMLElement, actions: Action[]) {
 	const forwarder = new ActionForwarder(element, actions);
 	return {
 		destroy: () => forwarder.onDestroy(),
-		update: (actions: Array<Action>) => forwarder.onUpdate(actions)
+		update: (actions: Action[]) => forwarder.onUpdate(actions)
 	};
 }
