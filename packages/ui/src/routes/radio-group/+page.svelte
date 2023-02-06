626e38c3f6766c9d33d3d875ef86e8d65d811956
Shawn Lee <changoloco.evg@gmail.com>
2023-02-06 14:44:06 -0600

    ui(route): update radio-group page

--- packages/ui/src/routes/radio-group/+page.svelte
+++ packages/ui/src/routes/radio-group/+page.svelte
@@ -1,86 +1,146 @@
 <script lang="ts">
-	import { RadioGroup, RadioGroupLabel, RadioGroupOption } from "$lib";
-	import { useClassNameResolver } from "$lib/hooks";
 	import { Page, Toggle } from "@app/components";
+	import { RadioGroup, RadioGroupDescription, RadioGroupLabel, RadioGroupOption } from "$lib";
+	import { useClassNameResolver } from "$lib/hooks";
+
+	let horizontal = false;
+	let value = "Halo 2";
 
 	const className = useClassNameResolver<"DISABLED" | "SELECTED">({
-		base: "p-6 | border-2 border-transparent",
-		selected: "border-raisin-20 rounded-lg text-white"
+		base: "px-6 py-2 | flex items-center justify-between | outline-none cursor-pointer focus:(ring-2 ring-white)",
+		disabled: "bg-raisin-12 opacity-50",
+		selected: { on: "bg-raisin-15 font-medium text-white", off: "bg-raisin-12" }
 	});
-
-	let value = "Ichigo";
-	let horizontal = false;
 </script>
 
 <Page title="Radio Group">
 	<svelte:fragment slot="options">
 		<Toggle bind:checked={horizontal} text="Toggle Horizontal" />
 	</svelte:fragment>
-
-	<span>Value: {value}</span>
-	<div class="grid grid-cols-2 gap-12">
-		<!-- item with same value is selected -->
-		<RadioGroup bind:value {horizontal}>
-			<RadioGroupLabel>Favourite Character</RadioGroupLabel>
-			<RadioGroupOption class={className} value="Naruto">
-				<RadioGroupLabel>Ninja Hero</RadioGroupLabel>
-				Naruto
+	<div class="grid grid-cols-4 items-start gap-6">
+		<RadioGroup class="grid gap-3" bind:value {horizontal}>
+			<RadioGroupLabel class="text-white">Best trilogy Bungie Halo game</RadioGroupLabel>
+			<RadioGroupOption class={className} value="Halo CE" let:isSelected>
+				<span> Halo Combat Evolved </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
 			</RadioGroupOption>
-			<RadioGroupOption class={className} value="Ichigo">
-				<RadioGroupLabel>Shinigami Hero</RadioGroupLabel>
-				Ichigo
+			<RadioGroupOption class={className} value="Halo 2" let:isSelected>
+				<span> Halo 2 </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
 			</RadioGroupOption>
-			<RadioGroupOption class={className} value="Luffy" selected>
-				<RadioGroupLabel>Pirate King</RadioGroupLabel>
-				Luffy
-			</RadioGroupOption>
-		</RadioGroup>
-		<!-- item with same value is selected -->
-		<RadioGroup value="Luffy" {horizontal}>
-			<RadioGroupLabel>Favourite Character</RadioGroupLabel>
-			<RadioGroupOption class={className} value="Naruto">
-				<RadioGroupLabel>Ninja Hero</RadioGroupLabel>
-				Naruto
-			</RadioGroupOption>
-			<RadioGroupOption class={className} value="Ichigo">
-				<RadioGroupLabel>Shinigami Hero</RadioGroupLabel>
-				Ichigo
-			</RadioGroupOption>
-			<RadioGroupOption class={className} value="Luffy">
-				<RadioGroupLabel>Pirate King</RadioGroupLabel>
-				Luffy
+			<RadioGroupOption class={className} value="Halo 3" let:isSelected>
+				<span> Halo 3 </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
 			</RadioGroupOption>
 		</RadioGroup>
-		<!-- the first selected option is taken -->
-		<RadioGroup {horizontal}>
-			<RadioGroupLabel>Favourite Character</RadioGroupLabel>
-			<RadioGroupOption class={className} value="Naruto">
-				<RadioGroupLabel>Ninja Hero</RadioGroupLabel>
-				Naruto
+		<RadioGroup class="grid gap-3" bind:value {horizontal}>
+			<RadioGroupLabel class="text-white">Best major Halo game</RadioGroupLabel>
+			<RadioGroupOption class={className} value="Halo Reach" disabled let:isSelected>
+				<span> Halo Reach </span>
+				<RadioGroupDescription class="sr-only">
+					5th videogame in the Halo series released in 2010.
+				</RadioGroupDescription>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
 			</RadioGroupOption>
-			<RadioGroupOption class={className} value="Ichigo" selected>
-				<RadioGroupLabel>Shinigami Hero</RadioGroupLabel>
-				Ichigo
+			<RadioGroupOption class={className} value="Halo CE" let:isSelected>
+				<span> Halo Combat Evolved </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
 			</RadioGroupOption>
-			<RadioGroupOption class={className} value="Luffy" selected>
-				<RadioGroupLabel>Pirate King</RadioGroupLabel>
-				Luffy
+			<RadioGroupOption class={className} value="Halo 2" let:isSelected>
+				<span> Halo 2 </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
+			</RadioGroupOption>
+			<RadioGroupOption class={className} value="Halo 3" let:isSelected>
+				<span> Halo 3 </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
+			</RadioGroupOption>
+			<RadioGroupOption class={className} value="Halo 4" disabled let:isSelected>
+				<span> Halo 4 </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
 			</RadioGroupOption>
 		</RadioGroup>
-		<!-- no item will be selected -->
-		<RadioGroup {horizontal}>
-			<RadioGroupLabel>Favourite Character</RadioGroupLabel>
-			<RadioGroupOption class={className} value="Naruto">
-				<RadioGroupLabel>Ninja Hero</RadioGroupLabel>
-				Naruto
+		<RadioGroup class="grid gap-3" {horizontal}>
+			<RadioGroupOption class={className} value="Halo CE" let:isSelected>
+				<span> Halo Combat Evolved </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
 			</RadioGroupOption>
-			<RadioGroupOption class={className} value="Ichigo">
-				<RadioGroupLabel>Shinigami Hero</RadioGroupLabel>
-				Ichigo
+			<RadioGroupOption class={className} value="Halo 2" let:isSelected>
+				<span> Halo 2 </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
 			</RadioGroupOption>
-			<RadioGroupOption class={className} value="Luffy">
-				<RadioGroupLabel>Pirate King</RadioGroupLabel>
-				Luffy
+			<RadioGroupOption class={className} value="Halo 3" selected let:isSelected>
+				<span> Halo 3 </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
+			</RadioGroupOption>
+		</RadioGroup>
+		<RadioGroup class="grid gap-3" {horizontal}>
+			<RadioGroupOption class={className} value="Halo CE" let:isSelected>
+				<span> Halo Combat Evolved </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
+			</RadioGroupOption>
+			<RadioGroupOption class={className} value="Halo 2" let:isSelected>
+				<span> Halo 2 </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
+			</RadioGroupOption>
+			<RadioGroupOption class={className} value="Halo 3" let:isSelected>
+				<span> Halo 3 </span>
+				{#if isSelected}
+					<div class="w-5 h-5 | grid place-content-center">
+						<i class="bx bxs-badge-check text-xl" />
+					</div>
+				{/if}
 			</RadioGroupOption>
 		</RadioGroup>
 	</div>
